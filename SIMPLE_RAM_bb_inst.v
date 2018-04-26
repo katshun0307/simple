da4fc3501384cf414679a64e351cadeb46fcfec9
YUYA SUMIE <monamonacan0926@gmail.com>
2018-04-26 17:55:54 +0900

    modify p4 files

--- SIMPLE_RAM_bb_inst.v
+++ SIMPLE_RAM_bb_inst.v
@@ -2,6 +2,7 @@ SIMPLE_RAM_bb	SIMPLE_RAM_bb_inst (
 	.address ( address_sig ),
 	.clock ( clock_sig ),
 	.data ( data_sig ),
+	.rden ( rden_sig ),
 	.wren ( wren_sig ),
 	.q ( q_sig )
 	);
--- p4.v
+++ p4.v
@@ -2,28 +2,24 @@ module p4(
 	input[15:0] address,
 	input[15:0] storeData,
 	input clock,
-	input[1:0] memWrite,
 	input writeReg,
 	input [2:0] regAddress,
+	input writeEnable,
+	input readEnable,
 	output reg [15:0] readOutData,
 	output reg WriteReg,
 	output reg [2:0]RegAddress
 	
 );
 
- reg writeEnable;
  wire[15:0] ROD;
  /*wire[2:0] RA;
  wire WR;*/
  
- if(memWrite == 2'b01) begin
-		writeEnable <= 0;
-	end else if(opcode == 2'b10) begin
-		writeEnable <= 1;
-	end
+
+
  
- 
- SIMPLE_RAM_bb BIGRAM(.address(address) , .clock(clock) , .data(storeData), .wren(writeEnable) , .q(ROD));
+ SIMPLE_RAM_bb BIGRAM(.address(address) , .clock(clock) , .data(storeData), .rden(readEnable) , .wren(writeEnable) , .q(ROD));
  
  always @(posedge clock) begin 
 	WriteReg <= writeReg;
