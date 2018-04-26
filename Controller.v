794715335001cced37af544cd5f6c8aaa2a21e50
Shuntaro Katsuda <katsuda.shuntaro.25s@st.kyoto-u.ac.jp>
2018-04-26 14:33:17 +0900

    added controller to send clock

---
+++ Controller.v
@@ -0,0 +1,27 @@
+module Controller (
+	input clock,
+	output clock0, clock1, clock2, clock3, clock4,
+	output reg [3:0] counter);
+	
+
+initial begin
+	counter <= 0;
+end
+
+always @(negedge clock) begin
+	// update counter
+	if (counter <= 3) begin
+		counter <= counter+1;
+	end else begin
+		counter <= 0;
+	end
+end
+
+// clock control
+assign clock0 = clock & (counter == 0);
+assign clock1 = clock & (counter == 1);
+assign clock2 = clock & (counter == 2);
+assign clock3 = clock & (counter == 3);
+assign clock4 = clock & (counter == 4);
+
+endmodule
