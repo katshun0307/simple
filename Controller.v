77c4c123ce346a7f84e750cdc34e5a5c396b4a76
Shuntaro Katsuda <katsuda.shuntaro.25s@st.kyoto-u.ac.jp>
2018-05-10 17:25:28 +0900

    removed exec button from controller

--- Controller.v
+++ Controller.v
@@ -11,41 +11,50 @@ reg preparestop;
 reg running;
 
 initial begin
-	counter = 4'b0000;
-	running = 1'b0;
+	counter <= 4'b0000;
+	running = 1'b1;
 end
 
 wire ispressed;
-chattercounter(.chatterclock(clock), .switchin(~execbutton), .ispressedout(ispressed));
+chattercounter(.chatterclock(clock), .switchin(execbutton), .ispressedout(ispressed));
 
 // loop through clock
-always @(negedge clock or posedge ispressed) begin
-	if (ispressed == 1) begin
-		preparestop <= 1;
-	end else begin
-	case (counter)
-		9: if (preparestop == 1) begin //switch status
-			preparestop <= 0;
-			running = ~running;
-			end else begin  // increment
-				counter = 0;
-			end
-		default: counter = counter + 4'b1; // increment
-	endcase
-	if (running == 1) begin
-		statusled = 8'b10011110;
-	end else begin
-		statusled = 8'b10110110;
-	end
-	end
-	
+//always @(negedge clock or posedge ispressed) begin
+//	if (ispressed == 1) begin
+//		preparestop <= 1;
+//	end else begin
+//		case (counter)
+//			9: if (preparestop == 1) begin //switch status
+//				preparestop <= 0;
+//				running = ~running;
+//				counter = 0;
+//				end else begin  // increment
+//					counter = 0;
+//				end
+//			default: counter = counter + 4'b1; // increment
+//		endcase
+//		
+//		if (running == 1) begin
+//			statusled = 8'b10011110;
+//		end else begin
+//			statusled = 8'b10110110;
+//		end
+//	end
+//	
 	// led clock
-	case (counter)
-	0: clockled = 4'b10000;
-	1: clockled = 4'b01000;
-	2: clockled = 4'b00100;
-	3: clockled = 4'b00010;
-	4: clockled = 4'b00001;
+//	case (counter)
+//	0: clockled = 4'b10000;
+//	1: clockled = 4'b01000;
+//	2: clockled = 4'b00100;
+//	3: clockled = 4'b00010;
+//	4: clockled = 4'b00001;
+//	endcase
+//end
+
+always @(posedge clock) begin
+	case(counter)
+		9: counter <= 0;
+		default: counter <= counter + 1;
 	endcase
 end
 
