ce068ebbc24ade3707ae48b0f090036e2d3e20e0
Shuntaro Katsuda <katsuda.shuntaro.25s@st.kyoto-u.ac.jp>
2018-05-25 10:51:28 +0900

    some updates

--- p2.v
+++ p2.v
@@ -283,16 +283,16 @@ if (writeflag == 1'b1) begin // if write to register
 	endcase
 end
 
-if(reset) begin
-	r0 <= 0;
-	r1 <= 0;
-	r2 <= 0;
-	r3 <= 0;
-	r4 <= 0;
-	r5 <= 0;
-	r6 <= 0;
-	r7 <= 0;
-end
+//if(reset) begin
+//	r0 <= 0;
+//	r1 <= 0;
+//	r2 <= 0;
+//	r3 <= 0;
+//	r4 <= 0;
+//	r5 <= 0;
+//	r6 <= 0;
+//	r7 <= 0;
+//end
 end
 
 endmodule
